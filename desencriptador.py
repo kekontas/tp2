18baf50f267ddb156c3efa78fd002c60211a5a29
Alejo <alejozimmermann@gmail.com>
2023-05-10 12:41:58 -0300

    commit

--- desencriptador.py
+++ desencriptador.py
@@ -1,33 +1,41 @@
-def desencriptador (encripted_file , clave):
-    """ 
+def desencriptador(encripted_file: str, clave: str):
+    """
     Pide al usuario una clave y un path para un archivo el cual desea que su contenido sea desencripado.
     Guarda el texto desencriptado en un nuevo archivo con un nombre elegido por el usuario.
     Si no existe el archivo que quiere desencriptar pide que ingresen un path hasta que exista el archivo con dicho path.
     La clave debe ser compuesta unicamente por letras de abecedario ingles minusculas.
+    La funcion no devuelve nada. Simplemente crea un archivo con el texto desecriptado.
     """
 
 
 
-    while True: #reviso si existe el path. si no existe, sigue preguntando hasta que exista. Uso el try-except por los Errores.
+    while True:  # reviso si existe el path. si no existe, sigue preguntando hasta que exista.
         try:
             with open(encripted_file, 'r'):
                 break
-        except FileNotFoundError:
+        except FileNotFoundError:  # atajo errores si no se encuentra el archivo.
             print('No existe el archivo.')
-            encripted_file = input('Ingrese el path de un archivo: ')
+            encripted_file = input('Ingrese el path de un archivo encriptado: ')
+        except PermissionError:  # atajo errores si el archivo no se puede abrir por permisos.
+            print("No se puede abrir un archivo que requiere permisos.")
+            encripted_file = input('Ingrese el path de un archivo encriptado: ')
+        except IsADirectoryError:  # atajo errores si el usuario ingresa un directorio.
+            print("Es un directorio.")
+            encripted_file = input('Ingrese el path de un archivo encriptado: ')
+
 
     
-    while clave.isalpha() is not True: #reviso que la contrasena este compuesta solamente por letras del abecedario ingles.
+    while clave.isalpha() is not True:  # reviso que la contrasena este compuesta solamente por letras del abecedario ingles.
         print('La clave solo puede contener letras del alfabeto inglés')
         clave = input('Ingrese una clave: ')
 
-    with open(encripted_file, 'rt') as encripted_file: #abro el file en modo de lectura porque simplemente me interesa la informacion del archivo
+    with open(encripted_file, 'rt') as encripted_file:  # abro el file en modo de lectura porque simplemente me interesa la informacion del archivo
 
-        destination = input('Cree un nombre para el archivo desencriptado: ') #pido el nombre del archivo donde se va a guardar el exto desencriptado.
-        if '.txt' not in destination: #si el usuario escribe el archivo si el '.txt' se lo agrego.
-            destination +='.txt'
+        destination = input('Cree un nombre para el archivo desencriptado: ')  # pido el nombre del archivo donde se va a guardar el exto desencriptado.
+        if '.txt' not in destination:  # si el usuario escribe el archivo si el '.txt' se lo agrego.
+            destination += '.txt'
 
-        with open (destination,'wt') as desencripted_file: #creo el archivo donde se va a guardar el texto desencriptado.
+        with open(destination, 'wt') as desencripted_file:  # creo el archivo donde se va a guardar el texto desencriptado.
             lineas = encripted_file.readlines()
     
             for linea in lineas:
@@ -47,6 +55,8 @@ def desencriptador (encripted_file , clave):
                     else: 
                         desencripted_line += chr(c + 97) 
                 desencripted_file.write(''.join(desencripted_line))
+
+
 encripted_file = input('Ingrese el path del archivo que desea desencriptar: ')
 clave = input('ingrese la clave: ')
-desencriptador(encripted_file,clave)
+desencriptador(encripted_file, clave)
--- encriptador.py
+++ encriptador.py
@@ -1,33 +1,39 @@
-def encriptador (file , clave):
-    """ 
-    Pide al usuariouna clave y un path para un archivo el cual desea que su contenido sea encripado.
+def encriptador(file: str, clave: str):
+    """
+    Pide al usuario una clave y un path para un archivo el cual desea que su contenido sea encripado.
     Guarda el texto encriptado en un nuevo archivo con un nombre elegido por el usuario.
     Si no existe el archivo pide que ingresen un path hasta que exista el archivo con dicho path.
     La clave debe ser compuesta unicamente por letras de abecedario ingles minusculas.
+    El archivo no devuelve nada. Solamente crea un archivo con el texto encriptado.
     """
 
 
 
-    while True: #reviso si existe el path. si no existe, sigue preguntando hasta que exista. Uso el try-except por los Errores.
+    while True:  # reviso si existe el path. si no existe, sigue preguntando hasta que exista.
         try:
             with open(file, 'r'):
                 break
-        except FileNotFoundError:
+        except FileNotFoundError:  # atajo errores si el archivo no puede ser encontrado
             print('No existe el archivo.')
             file = input('Ingrese el path de un archivo: ')
-
+        except PermissionError:  # atajo errores si el archivo no se puede abrir por permisos.
+            print("No se puede abrir un archivo que requiere permisos.")
+            file = input('Ingrese el path de un archivo encriptado: ')
+        except IsADirectoryError:  # atajo errores si el usuario ingresa un directorio.
+            print("Es un directorio.")
+            file = input('Ingrese el path de un archivo encriptado: ')
     
-    while clave.isalpha() is not True: #reviso que la contrasena este compuesta solamente por letras del abecedario ingles.
+    while clave.isalpha() is not True:  # reviso que la contrasena este compuesta solamente por letras del abecedario ingles.
         print('La clave solo puede contener letras del alfabeto inglés')
         clave = input('Ingrese una clave: ')
 
-    with open(file, 'rt') as archivo: #abro el file en modo de lectura porque simplemente me interesa la informacion del archivo
+    with open(file, 'rt') as archivo:  # abro el file en modo de lectura porque simplemente me interesa la informacion del archivo
 
-        destination = input('Cree un nombre para el archivo encriptado: ') #pido el nombre del archivo donde se va a guardar el exto encriptado.
+        destination = input('Cree un nombre para el archivo encriptado: ')  # pido el nombre del archivo donde se va a guardar el exto encriptado.
         if '.txt' not in destination:
-            destination +='.txt'
+            destination += '.txt'
 
-        with open (destination,'wt') as encripted_archive: #creo el archivo donde se va a guardar el texto encriptado.
+        with open(destination, 'wt') as encripted_archive:  # creo el archivo donde se va a guardar el texto encriptado.
             lineas = archivo.readlines()
     
             for linea in lineas:
@@ -46,6 +52,7 @@ def encriptador (file , clave):
                         encripted_line += chr(c + 97) 
                 encripted_archive.write(''.join(encripted_line))
 
+
 file = input('Ingrese el path del archivo: ')
 clave = input('ingrese la clave: ')
-encriptador(file,clave)
+encriptador(file, clave)
